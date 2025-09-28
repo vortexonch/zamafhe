bfa6e3568f1303c0c4e0ff32a6c092868dc8714f
favdev10 <favdev10@gmail.com>
2025-09-28 23:46:57 +0100

    Landing page complete

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Simple JS placeholder
-document.getElementById("chatBtn").addEventListener("click", () => {
-  alert("Chatbot coming soon 🚀");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("subscribeForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // stop reload
+    alert("🎉 Congratulations! You’ve successfully subscribed.");
+    form.reset(); // clear input after submit
+  });
 });
